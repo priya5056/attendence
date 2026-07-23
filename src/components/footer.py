2dd366b7bf3f2ab2252baa9fc5c93ecb0283837a
pp <146319574+priya5056@users.noreply.github.com>
2026-07-23 09:14:42 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -17,8 +17,7 @@ def footer_dashboard():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style=="font-weight:bold; color:black;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:25px' />
+        <p style=="font-weight:bold; color:black;"> Created with ❤️ by />
         </div>
                 
                 """, unsafe_allow_html=True)
