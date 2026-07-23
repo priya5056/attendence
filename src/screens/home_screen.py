a7138b2618a7d08d780c9d073c81553347362144
pp <146319574+priya5056@users.noreply.github.com>
2026-07-23 09:23:40 +0530

    Update home_screen.py

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -10,20 +10,38 @@ def home_screen():
     style_base_layout()
 
 
-    col1, col2 = st.columns(2, gap="large")
+  col1, col2 = st.columns(2, gap="large")
 
-    with col1:
-        st.header("I'm Student")
-        st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
-        if st.button('Student Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
-            st.session_state['login_type']='student'
-            st.rerun()
+with col1:
+    st.markdown(
+        "<h2 style='color: black;'>I'm Student</h2>",
+        unsafe_allow_html=True
+    )
+    st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
 
-    with col2:
-        st.header("I'm Teacher")
-        st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=145)
-        if st.button('Teacher Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
-            st.session_state['login_type']='teacher'
-            st.rerun()
+    if st.button(
+        'Student Portal',
+        type='primary',
+        icon=':material/arrow_outward:',
+        icon_position='right'
+    ):
+        st.session_state['login_type'] = 'student'
+        st.rerun()
 
-    footer_home()
+with col2:
+    st.markdown(
+        "<h2 style='color: black;'>I'm Teacher</h2>",
+        unsafe_allow_html=True
+    )
+    st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=145)
+
+    if st.button(
+        'Teacher Portal',
+        type='primary',
+        icon=':material/arrow_outward:',
+        icon_position='right'
+    ):
+        st.session_state['login_type'] = 'teacher'
+        st.rerun()
+
+footer_home()
