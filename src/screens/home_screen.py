41bfaacae016e008a5e56c85a8e6db02fd930e6a
pp <146319574+priya5056@users.noreply.github.com>
2026-07-23 09:27:21 +0530

    Update home_screen.py

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -2,46 +2,46 @@ import streamlit as st
 from src.components.header import header_home
 from src.components.footer import footer_home
 from src.ui.base_layout import style_base_layout, style_background_home
-def home_screen():
 
+
+def home_screen():
 
     header_home()
     style_background_home()
     style_base_layout()
 
+    col1, col2 = st.columns(2, gap="large")
 
-  col1, col2 = st.columns(2, gap="large")
+    with col1:
+        st.markdown(
+            "<h2 style='color:black;'>I'm Student</h2>",
+            unsafe_allow_html=True,
+        )
+        st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
 
-with col1:
-    st.markdown(
-        "<h2 style='color: black;'>I'm Student</h2>",
-        unsafe_allow_html=True
-    )
-    st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
+        if st.button(
+            "Student Portal",
+            type="primary",
+            icon=":material/arrow_outward:",
+            icon_position="right",
+        ):
+            st.session_state["login_type"] = "student"
+            st.rerun()
 
-    if st.button(
-        'Student Portal',
-        type='primary',
-        icon=':material/arrow_outward:',
-        icon_position='right'
-    ):
-        st.session_state['login_type'] = 'student'
-        st.rerun()
+    with col2:
+        st.markdown(
+            "<h2 style='color:black;'>I'm Teacher</h2>",
+            unsafe_allow_html=True,
+        )
+        st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=145)
 
-with col2:
-    st.markdown(
-        "<h2 style='color: black;'>I'm Teacher</h2>",
-        unsafe_allow_html=True
-    )
-    st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=145)
+        if st.button(
+            "Teacher Portal",
+            type="primary",
+            icon=":material/arrow_outward:",
+            icon_position="right",
+        ):
+            st.session_state["login_type"] = "teacher"
+            st.rerun()
 
-    if st.button(
-        'Teacher Portal',
-        type='primary',
-        icon=':material/arrow_outward:',
-        icon_position='right'
-    ):
-        st.session_state['login_type'] = 'teacher'
-        st.rerun()
-
-footer_home()
+    footer_home()
